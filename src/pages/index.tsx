d1e7f1e52e52e628462bbcf328c300be79b1c49f
ciocan <ciocan@gmail.com>
2020-11-12 15:05:09 +0000

    add graphql

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,28 @@ import React, { useEffect, useState } from 'react'
 import { Text, Box } from '@chakra-ui/core'
 import { useRouter } from 'next/router'
 
+import { GraphQLClient } from 'graphql-request'
+import useSWR from 'swr'
+
 import { useUser, useFirstRender } from 'hooks'
+import { API_URL } from 'lib/config'
+
+const QUERY = `
+  query sayHello {
+    sayHello
+  }
+`
+
+const client = new GraphQLClient(`${API_URL}/graphql`)
 
 export default function Home(): JSX.Element {
   const router = useRouter()
   const [initialized, setInitialized] = useState(false)
   const isFirstRender = useFirstRender()
   const { user, loading: userLoading } = useUser()
+
+  const { data, error } = useSWR(QUERY, (query) => client.request(query))
+  console.log(data, error)
 
   useEffect(() => {
     if (user && !userLoading && !initialized) {
