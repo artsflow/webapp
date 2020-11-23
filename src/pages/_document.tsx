d87bfe0581d13ba93d33dc1d6470a1ddf2010705
ciocan <ciocan@gmail.com>
2020-11-23 14:53:58 +0000

    preload google maps api url

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,19 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 import React from 'react'
 
+import { GCP_MAPS_KEY } from 'lib/config'
+
 export default class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
+          <script
+            type="text/javascript"
+            src={`https://maps.google.com/maps/api/js?key=${GCP_MAPS_KEY}`}
+            async
+          />
         </Head>
         <style>
           {`
