ae1bb2909dec4b3715eccb27bed1e1eaddefc701
ciocan <ciocan@gmail.com>
2020-11-23 13:15:45 +0000

    add address lookup (1/3)

--- src/pages/add.tsx
+++ src/pages/add.tsx
@@ -1,22 +1,30 @@
+import { useState, useCallback } from 'react'
 import { gql } from 'graphql-request'
 import { useForm } from 'react-hook-form'
 import {
+  Box,
   Text,
+  Heading,
   Button,
   Input,
   Textarea,
   VStack,
+  HStack,
   FormErrorMessage,
   FormLabel,
   FormControl,
+  List,
+  ListItem,
 } from '@chakra-ui/core'
+import { SearchIcon } from '@chakra-ui/icons'
+import { isEmpty } from 'lodash'
 
 import { Meta } from 'components'
 import { client } from 'services/client'
 
 const ADD_SERVICE = gql`
-  mutation addService($serviceInput: ServiceInput!) {
-    addService(serviceInput: $serviceInput)
+  mutation addService($input: ServiceInput!) {
+    addService(input: $input)
   }
 `
 
@@ -24,7 +32,8 @@ export default function AddService() {
   const { register, handleSubmit, errors, formState } = useForm()
   const onSubmit = async (data: any) => {
     console.log(data)
-    const variables = { serviceInput: { ...data, published: false } }
+    if (!data) return
+    const variables = { input: { ...data, published: false } }
     const res = await client.request(ADD_SERVICE, variables)
     console.log(res)
   }
@@ -32,24 +41,29 @@ export default function AddService() {
   return (
     <>
       <Meta title="Add service" />
-      <Text>Add service</Text>
+      <Heading size="md">Add service</Heading>
       <form onSubmit={handleSubmit(onSubmit)}>
         <FormControl isInvalid={errors.name}>
-          <VStack mt="8" w="md" alignItems="flex-start">
-            <FormLabel htmlFor="title">Title</FormLabel>
-            <Input
-              name="title"
-              placeholder="Add title"
-              size="md"
-              ref={register({ required: true, maxLength: 80 })}
-            />
-            <FormLabel htmlFor="description">Description</FormLabel>
-            <Textarea
-              name="description"
-              placeholder="Add description"
-              size="md"
-              ref={register({ required: true, maxLength: 200 })}
-            />
+          <VStack mt="8" w="md" alignItems="flex-start" spacing="4">
+            <Box w="full">
+              <FormLabel htmlFor="title">Title</FormLabel>
+              <Input
+                name="title"
+                placeholder="Add title"
+                size="md"
+                ref={register({ required: true, maxLength: 80 })}
+              />
+            </Box>
+            <Box w="full">
+              <FormLabel htmlFor="description">Description</FormLabel>
+              <Textarea
+                name="description"
+                placeholder="Add description"
+                size="md"
+                ref={register({ required: true, maxLength: 200 })}
+              />
+            </Box>
+            <AddressLookup />
             <Button type="submit" isLoading={formState.isSubmitting}>
               Submit
             </Button>
@@ -60,3 +74,87 @@ export default function AddService() {
     </>
   )
 }
+
+const GET_ADDRESS = gql`
+  query getAddress($input: String!) {
+    getAddress(input: $input) {
+      id
+      streetNumber
+      streetName
+      municipality
+      extendedPostalCode
+      country
+      countryCode
+      freeformAddress
+      position {
+        lat
+        lon
+        geohash
+      }
+    }
+  }
+`
+
+const AddressLookup = () => {
+  const [selectedAddress, setSelectedAddress]: any = useState({})
+  const [lookupAddress, setLookupAddress] = useState('')
+  const [addresses, setAddresses] = useState([])
+  const [isLoading, setLoading] = useState(false)
+
+  const handleClick = useCallback(async () => {
+    if (!lookupAddress) return
+    setLoading(true)
+    const result = await client.request(GET_ADDRESS, { input: lookupAddress })
+    setAddresses(result.getAddress)
+    setSelectedAddress({})
+    setLoading(false)
+  }, [lookupAddress])
+
+  const showList = addresses.length > 0 && isEmpty(selectedAddress)
+
+  return (
+    <Box w="full">
+      <FormLabel htmlFor="address">Address lookup</FormLabel>
+      <HStack>
+        <Input
+          placeholder="Search address"
+          value={lookupAddress}
+          onChange={(e) => setLookupAddress(e.target.value)}
+          size="md"
+        />
+        <Button onClick={handleClick} isLoading={isLoading}>
+          <SearchIcon />
+        </Button>
+      </HStack>
+      {!isEmpty(selectedAddress) && (
+        <Box mt="2" p="2">
+          <AddressInfo {...selectedAddress} />
+        </Box>
+      )}
+      {showList && (
+        <List spacing="2">
+          {addresses.map((a: any) => (
+            <ListItem key={a.id}>
+              <Button variant="outline" onClick={() => setSelectedAddress(a)}>
+                {a.freeformAddress}
+              </Button>
+            </ListItem>
+          ))}
+        </List>
+      )}
+    </Box>
+  )
+}
+
+const AddressInfo = ({
+  streetNumber,
+  streetName,
+  extendedPostalCode,
+  municipality,
+  countryCode,
+  country,
+}: any) => (
+  <Text>
+    {streetNumber} {streetName}, {municipality}, {extendedPostalCode}, {countryCode}, {country}{' '}
+  </Text>
+)
