aa91323cdebd9ec9fc92d9e01de02ac0fc6a33d6
ciocan <ciocan@gmail.com>
2020-11-25 13:49:02 +0000

    add upload component - preview & delete (2/2)

--- src/components/ImageUploader/index.tsx
+++ src/components/ImageUploader/index.tsx
@@ -1,7 +1,8 @@
-import { useCallback } from 'react'
+import { useState, useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
 import { RiImageAddFill } from 'react-icons/ri'
-import { Icon } from '@chakra-ui/core'
+import { Box, Icon, Spinner, Image, IconButton } from '@chakra-ui/core'
+import { DeleteIcon } from '@chakra-ui/icons'
 import { gql } from 'graphql-request'
 
 import { client } from 'services/client'
@@ -12,10 +13,27 @@ const UPLOAD_IMAGE = gql`
   }
 `
 
+const DELETE_IMAGE = gql`
+  mutation deleteImage($id: String!) {
+    deleteImage(id: $id)
+  }
+`
+
 export function ImageUploader() {
+  const [fileName, setFileName] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleDelete = useCallback(async () => {
+    console.log('remove', fileName)
+    setFileName('')
+    await client.request(DELETE_IMAGE, { id: fileName })
+  }, [fileName])
+
   const onDrop = useCallback(async ([file]) => {
-    console.log(file)
+    setIsLoading(true)
     const res = await client.request(UPLOAD_IMAGE, { file })
+    setIsLoading(false)
+    setFileName(res?.uploadImage)
     console.log(res)
   }, [])
 
@@ -25,9 +43,36 @@ export function ImageUploader() {
   })
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      <Icon w="100px" h="100px" as={RiImageAddFill} color="grey" />
-    </div>
+    <>
+      {fileName ? (
+        <Box pos="relative">
+          <Image
+            boxSize="100px"
+            src={`https://ik.imagekit.io/artsflow/${fileName}?tr=h-100,w-100`}
+          />
+          <IconButton
+            top="3"
+            right="0"
+            pos="absolute"
+            variant="link"
+            colorScheme="red"
+            aria-label="Delete"
+            icon={<DeleteIcon />}
+            onClick={handleDelete}
+          />
+        </Box>
+      ) : (
+        <Box {...getRootProps()} outline="none" cursor="pointer">
+          {!isLoading ? (
+            <>
+              <input {...getInputProps()} />
+              <Icon w="100px" h="100px" as={RiImageAddFill} color="grey" />
+            </>
+          ) : (
+            <Spinner size="xl" />
+          )}
+        </Box>
+      )}
+    </>
   )
 }
