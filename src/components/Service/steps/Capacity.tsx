65f0a6d3a7416ea4d2592015a7d0fe2955c5f46f
ciocan <ciocan@gmail.com>
2020-12-02 08:22:25 +0000

    add capacity

--- src/components/Service/steps/Capacity.tsx
+++ src/components/Service/steps/Capacity.tsx
@@ -1,12 +1,39 @@
-import { Box, Text, Heading } from '@chakra-ui/core'
+import { useContext } from 'react'
+
+import {
+  Box,
+  Heading,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+} from '@chakra-ui/core'
+
+import { Context } from '../machine'
+import { Lipsum } from '../config'
 
 export function Capacity() {
+  const { send, context } = useContext(Context) as any
+  const { capacity } = context
+
+  const handleChange = (_: string, value: number) => {
+    send({ type: 'UPDATE', data: { capacity: value, meta: { isDirty: true } } })
+  }
+
   return (
     <Box w="full">
       <Heading mb="4" size="lg">
         Capacity
       </Heading>
-      <Text>not implemented</Text>
+      <NumberInput onChange={handleChange} defaultValue={capacity} min={1} max={30}>
+        <NumberInputField />
+        <NumberInputStepper>
+          <NumberIncrementStepper />
+          <NumberDecrementStepper />
+        </NumberInputStepper>
+      </NumberInput>
+      <Lipsum />
     </Box>
   )
 }
