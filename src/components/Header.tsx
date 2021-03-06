b3b02262b99c362cd2d4890100f90a9ef82f19ae
ciocan <ciocan@gmail.com>
2021-03-06 17:34:09 +0000

    add menu icon buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,16 +12,26 @@ import {
   MenuList,
   MenuItem,
   MenuDivider,
+  HStack,
+  IconButton,
+  Icon,
+  useDisclosure,
+  Badge,
 } from '@chakra-ui/react'
 import { ChevronDownIcon } from '@chakra-ui/icons'
 import NextLink from 'next/link'
 
 import Logo from 'svg/artsflow.svg'
+import BellIcon from 'svg/icons/bell.svg'
+import ChatIcon from 'svg/icons/chat.svg'
+
 import { auth } from 'lib/firebase'
 import { useRouter } from 'next/router'
 import { UserContext } from 'lib/context'
+import { Notifications } from 'components'
 
 export function Header() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const { user } = useContext(UserContext)
   const router = useRouter()
 
@@ -35,7 +45,7 @@ export function Header() {
       as="nav"
       py="4"
       px="2rem"
-      gridTemplateColumns="auto 1fr auto"
+      gridTemplateColumns="auto 1fr auto auto"
       alignItems="center"
       boxShadow="0px 3px 8px -1px rgba(50, 50, 71, 0.05)"
       zIndex="1"
@@ -47,7 +57,11 @@ export function Header() {
           </Link>
         </NextLink>
       </Box>
-      <VStack ml="auto" mr="4" spacing="0" alignItems="flex-end">
+      <HStack ml="auto" spacing="1rem" borderRight="1px solid #ECEDF1" mr="1.5rem" pr="1.5rem">
+        <RoundButton icon={<Icon as={ChatIcon} />} onClick={() => router.push('/chat')} />
+        <RoundButton icon={<Icon as={BellIcon} />} onClick={onOpen} />
+      </HStack>
+      <VStack mr="4" spacing="0" alignItems="flex-end">
         <Text fontSize="14px" fontWeight="bold">
           {user.displayName}
         </Text>
@@ -66,11 +80,42 @@ export function Header() {
           <Avatar name={user.displayName} width="36px" height="36px" src={user.photoURL} />
         </MenuButton>
         <MenuList>
-          <MenuItem>My Profile</MenuItem>
+          <MenuItem onClick={() => router.push('/profile')}>My Profile</MenuItem>
           <MenuDivider />
           <MenuItem onClick={handleLogout}>Log out</MenuItem>
         </MenuList>
       </Menu>
+      <Notifications onClose={onClose} isOpen={isOpen} />
     </Grid>
   )
 }
+
+const RoundButton = ({ icon, onClick }: any) => (
+  <Box position="relative">
+    <Badge
+      ml="1"
+      fontSize="10px"
+      bg="af.pink"
+      zIndex="1"
+      color="white"
+      pos="absolute"
+      right="-5px"
+      rounded="full"
+      minW="16px"
+      minH="16px"
+      justifyContent="center"
+      display="flex"
+    >
+      3
+    </Badge>
+    <IconButton
+      variant="ghost"
+      border="1px solid #EEEEEE"
+      colorScheme="ghost"
+      aria-label="Send email"
+      isRound
+      icon={icon}
+      onClick={onClick}
+    />
+  </Box>
+)
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Text, Box } from '@chakra-ui/react'
 
-export default function Home(): JSX.Element {
+export default function Profile(): JSX.Element {
   return (
     <Box>
-      <Text>welcome</Text>
+      <Text>my profile</Text>
     </Box>
   )
 }
