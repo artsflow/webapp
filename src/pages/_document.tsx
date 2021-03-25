89c80a251fbe40e6c3b82c15393777adc12b80eb
ciocan <ciocan@gmail.com>
2021-03-25 13:41:12 +0000

    add favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,6 +23,10 @@ export default class MyDocument extends Document {
             src={`https://maps.google.com/maps/api/js?key=${GCP_MAPS_KEY}&libraries=places`}
             async
           />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <style>
           {`
