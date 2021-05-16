5f505d3ac7dc94277efeebcdefb174b8e364b08c
ciocan <ciocan@gmail.com>
2021-05-16 15:49:43 +0100

    add analytics to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,10 @@ import 'components/Calendar/styles.css'
 
 NProgress.configure({ showSpinner: false })
 Router.events.on('routeChangeStart', () => NProgress.start())
-Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeComplete', (url) => {
+  NProgress.done()
+  window.analytics?.page(url)
+})
 Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
