26e162920a99d48671b7c0adf475f71c44491118
ciocan <ciocan@gmail.com>
2021-05-16 15:42:35 +0100

    remove analytics from _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,10 +12,7 @@ import 'components/Calendar/styles.css'
 
 NProgress.configure({ showSpinner: false })
 Router.events.on('routeChangeStart', () => NProgress.start())
-Router.events.on('routeChangeComplete', (url) => {
-  NProgress.done()
-  window.analytics?.page(url)
-})
+Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
