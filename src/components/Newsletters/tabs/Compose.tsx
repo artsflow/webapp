d691d0ddc246b6a8e3a646cda72b88a1bda29d1b
ciocan <ciocan@gmail.com>
2021-05-24 16:06:20 +0100

    update composer

--- src/components/Newsletters/tabs/Compose.tsx
+++ src/components/Newsletters/tabs/Compose.tsx
@@ -1,6 +1,36 @@
-import { Text } from '@chakra-ui/react'
+import { Text, VStack, Input } from '@chakra-ui/react'
+import { useForm } from 'react-hook-form'
+
+import { Editor } from '../Editor'
 
 export const Compose = () => {
-  console.log('compose')
-  return <Text>compose content</Text>
+  const { register, handleSubmit } = useForm({})
+
+  const onSubmit = async (data: any) => {
+    console.log(data)
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <VStack alignItems="flex-start" pos="relative" spacing="0.5rem" pt="1.5rem">
+        <Text fontSize="sm" fontWeight="semibold">
+          Subject
+        </Text>
+        <Input
+          bg="white"
+          border="1px solid white"
+          shadow="0px 3px 8px rgba(50, 50, 71, 0.05)"
+          rounded="6px"
+          w="360px"
+          placeholder="Enter subject..."
+          ref={register({
+            required: true,
+            maxLength: 100,
+          })}
+          name="subject"
+        />
+        <Editor />
+      </VStack>
+    </form>
+  )
 }
