cce92877a42531514fe3a073b73a077c1d993ec4
ciocan <ciocan@gmail.com>
2021-05-27 14:16:38 +0100

    refactor editor

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,13 +1,7 @@
 import React, { useState } from 'react'
 import { BangleEditor, useEditorState } from '@bangle.dev/react'
-import { PluginKey, Plugin } from '@bangle.dev/core'
-import { corePlugins, coreSpec } from '@bangle.dev/core/utils/core-components'
-import { toHTMLString } from '@bangle.dev/core/utils/pm-utils'
-import { emoji } from '@bangle.dev/emoji'
-import { emojiSuggest, EmojiSuggest } from '@bangle.dev/react-emoji-suggest'
-import gemojiData from 'emoji-lookup-data/data/gemoji.json'
+import { EmojiSuggest } from '@bangle.dev/react-emoji-suggest'
 import {
-  floatingMenu,
   StaticMenu,
   Menu,
   HeadingButton,
@@ -29,59 +23,13 @@ import '@bangle.dev/tooltip/style.css'
 import '@bangle.dev/react-menu/style.css'
 import '@bangle.dev/react-emoji-suggest/style.css'
 
-const menuKey = new PluginKey('menuKey')
-const emojiSuggestKey = new PluginKey('emojiSuggestKey')
+import { getEditorConfig, emojiSuggestKey } from './utils'
 
 const CMenu = chakra(Menu)
 
 export function Editor({ onChange }: { onChange: (html: string) => void }) {
   const [editor, setEditor] = useState()
-  const editorState = useEditorState({
-    specs: [
-      ...coreSpec(),
-      emoji.spec({
-        getEmoji: (emojiAlias: any) => getEmojiByAlias(emojiAlias) || ['question', '❓'],
-      }),
-      emojiSuggest.spec({ markName: 'emojiSuggest' }),
-    ],
-    plugins: () => [
-      corePlugins(),
-      floatingMenu.plugins({
-        key: menuKey,
-      }),
-      new Plugin({
-        view: () => ({
-          update: (view: any, prevState: any) => {
-            if (!view.state.doc.eq(prevState.doc)) {
-              onChange(toHTMLString(view.state))
-            }
-          },
-        }),
-      }),
-      emoji.plugins(),
-      emojiSuggest.plugins({
-        key: emojiSuggestKey,
-        getEmojiGroups: (queryText: string) => {
-          if (!queryText) {
-            return emojiData
-          }
-          return emojiData
-            .map((group: any) => ({
-              name: group.name,
-              emojis: group.emojis.filter(([emojiAlias]: [string]) =>
-                emojiAlias.includes(queryText)
-              ),
-            }))
-            .filter((group: any) => group.emojis.length > 0)
-        },
-        markName: 'emojiSuggest',
-        tooltipRenderOpts: {
-          placement: 'bottom',
-        },
-      }),
-    ],
-    initialValue: ``,
-  })
+  const editorState = useEditorState(getEditorConfig(onChange))
 
   return (
     <Box
@@ -102,7 +50,7 @@ export function Editor({ onChange }: { onChange: (html: string) => void }) {
             borderBottomColor="blackAlpha.100"
             borderBottomWidth="1px"
             rounded="0"
-            px="0.5rem"
+            p="0.5rem"
           >
             <MenuGroup>
               <UndoButton />
@@ -129,25 +77,4 @@ export function Editor({ onChange }: { onChange: (html: string) => void }) {
       </BangleEditor>
     </Box>
   )
-}
-
-const emojiData = Object.values(
-  gemojiData.reduce((prev: any, obj) => {
-    if (!prev[obj.category]) {
-      prev[obj.category] = { name: obj.category, emojis: [] }
-    }
-    prev[obj.category].emojis.push([obj.aliases[0], obj.emoji])
-
-    return prev
-  }, {})
-) as any
-
-const getEmojiByAlias = (emojiAlias: any) => {
-  for (const { emojis } of emojiData) {
-    const match = emojis.find((e: any) => e[0] === emojiAlias)
-    if (match) {
-      return match
-    }
-  }
-  return null
 }
---
+++ src/components/Editor/utils.ts
@@ -0,0 +1,76 @@
+import { Plugin, PluginKey } from '@bangle.dev/core'
+import { corePlugins, coreSpec } from '@bangle.dev/core/utils/core-components'
+import { toHTMLString } from '@bangle.dev/core/utils/pm-utils'
+import { emoji } from '@bangle.dev/emoji'
+import { emojiSuggest } from '@bangle.dev/react-emoji-suggest'
+import { floatingMenu } from '@bangle.dev/react-menu'
+import gemojiData from 'emoji-lookup-data/data/gemoji.json'
+
+const menuKey = new PluginKey('menuKey')
+export const emojiSuggestKey = new PluginKey('emojiSuggestKey')
+
+export const getEditorConfig = (onChange: any) => ({
+  specs: [
+    ...coreSpec(),
+    emoji.spec({
+      getEmoji: (emojiAlias: any) => getEmojiByAlias(emojiAlias) || ['question', '❓'],
+    }),
+    emojiSuggest.spec({ markName: 'emojiSuggest' }),
+  ],
+  plugins: () => [
+    corePlugins(),
+    floatingMenu.plugins({
+      key: menuKey,
+    }),
+    new Plugin({
+      view: () => ({
+        update: (view: any, prevState: any) => {
+          if (!view.state.doc.eq(prevState.doc)) {
+            onChange(toHTMLString(view.state))
+          }
+        },
+      }),
+    }),
+    emoji.plugins(),
+    emojiSuggest.plugins({
+      key: emojiSuggestKey,
+      getEmojiGroups: (queryText: string) => {
+        if (!queryText) {
+          return emojiData
+        }
+        return emojiData
+          .map((group: any) => ({
+            name: group.name,
+            emojis: group.emojis.filter(([emojiAlias]: [string]) => emojiAlias.includes(queryText)),
+          }))
+          .filter((group: any) => group.emojis.length > 0)
+      },
+      markName: 'emojiSuggest',
+      tooltipRenderOpts: {
+        placement: 'bottom',
+      },
+    }),
+  ],
+  initialValue: ``,
+})
+
+const emojiData = Object.values(
+  gemojiData.reduce((prev: any, obj) => {
+    if (!prev[obj.category]) {
+      prev[obj.category] = { name: obj.category, emojis: [] }
+    }
+    prev[obj.category].emojis.push([obj.aliases[0], obj.emoji])
+
+    return prev
+  }, {})
+) as any
+
+const getEmojiByAlias = (emojiAlias: any) => {
+  for (const { emojis } of emojiData) {
+    const match = emojis.find((e: any) => e[0] === emojiAlias)
+    if (match) {
+      return match
+    }
+  }
+  return null
+}
