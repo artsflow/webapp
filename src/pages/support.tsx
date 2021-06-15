e22a9702026e12c018c524ecedcbdbba21bc97ca
ciocan <ciocan@gmail.com>
2021-06-15 13:41:28 +0100

    update support page copy

--- src/pages/support.tsx
+++ src/pages/support.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, Box } from '@chakra-ui/react'
+import { Text, Box, Heading } from '@chakra-ui/react'
 
 import { Meta } from 'components'
 
@@ -8,7 +8,12 @@ export default function Support(): JSX.Element {
     <>
       <Meta title="Support" />
       <Box p="40px">
-        <Text>Support coming soon...</Text>
+        <Heading fontSize="lg" mb="1rem">
+          Get support
+        </Heading>
+        <Text>
+          Use the chat bubble from the bottom right corner of the screen to get in contact with us.
+        </Text>
       </Box>
     </>
   )
