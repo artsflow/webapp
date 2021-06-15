80063b176726bc305f53fd687fbda9df110fbf07
ciocan <ciocan@gmail.com>
2021-06-15 09:48:08 +0100

    add feedback

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,3 +2,4 @@ import { firebaseCallable } from 'lib/firebase'
 
 export const updateUserVerification = () => firebaseCallable('updateUserVerification', {})
 export const checkUserFirstTime = () => firebaseCallable('checkUserFirstTime', {})
+export const addFeedback = (data: any) => firebaseCallable('addFeedback', data)
